f2987b23e6ecedd0fbba62118c3033b732cfbef1
jfelic <felicianojulian1@gmail.com>
2024-11-08 16:38:07 -0500

    Added Timer object to handle countdown logic in ApplicationState.
    We want to handle it here and not in session.dart so that the user can navigate between screens during a session.

--- lib/utilities/app_state.dart
+++ lib/utilities/app_state.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
+import 'dart:async';
 
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
@@ -14,22 +15,72 @@ class ApplicationState extends ChangeNotifier {
   bool _loggedIn = false;
   bool get loggedIn => _loggedIn;
 
-  // App-wide State
+  // App-wide State fields
   int stopwatchHours = 1;
   int stopwatchMinutes = 45; 
 
-  // Methods for Session
+  // Timer related fields
+  Timer? _sessionTimer;
+  Duration _remainingTime = Duration.zero;
+
+  Duration get remainingTime => _remainingTime;
+
+  /* Methods for Timer */
+  // Start the Timer
+  void startSession() {
+    final sessionDuration = Duration(hours: stopwatchHours, minutes: stopwatchMinutes);
+    _remainingTime = sessionDuration;
+
+    // Cancel any exitsting timers
+    _sessionTimer?.cancel();
+
+    // Timer will run every second
+    _sessionTimer = Timer.periodic(const Duration(seconds: 1), (timer){
+      // all of this happens every second:
+      if(_remainingTime.inSeconds <= 0) { // if timer is done
+        timer.cancel(); // Stop timer
+        _onSessionComplete(); // Handle end of session
+      } else { // timer not done yet
+        _remainingTime -= const Duration(seconds: 1); // Decrement remaining time
+        notifyListeners(); // Notify UI of changes
+      }
+    });
+  }
+
+  // Pause the session
+  void pauseSession() {
+    // TODO: Find a way to pause session without completely cancelling
+  } // pauseSession() end
+
+  // Stop the session
+  void stopSession() {
+    _sessionTimer?.cancel();
+    _remainingTime = Duration.zero; // Reset remaining time
+    notifyListeners();
+  } // stopSession() end
+
+  // Handle the end of a session
+  // Private function
+  void _onSessionComplete() {
+    // TODO: Handle when the session ends
+  } // _onSessionComplete() end
+
+  /* End of methods for Timer */
+
+  /* Methods for Session */
   // Update stopwatchHour based on user's session length
   void updateStopwatchHours(double newStopwatchHours) {
     stopwatchHours = newStopwatchHours.toInt();
     notifyListeners();
-  }
+  } // updateStopwatchHours() end
 
   // Update stopwatchHour based on user's session length
   void updateStopwatchMinutes(double newStopwatchMinutes) {
     stopwatchMinutes = newStopwatchMinutes.toInt();
     notifyListeners();
-  }
+  } // updateStopWatchMinutes() end
+
+  /* End of methods for Session */
 
 
   Future<void> init() async {

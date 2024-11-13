6f9cff6caef5ca462fcbd4dbc17dc09e4603f8c5
Joseph Heindel <heindelj@g.cofc.edu>
2024-11-13 07:57:19 -0500

    logout button widget

---
+++ lib/widgets/logout_button.dart
@@ -0,0 +1,23 @@
+// lib/widgets/logout_button.dart
+
+import 'package:flutter/material.dart';
+import 'package:leaf_n_lit/utilities/auth_service.dart';
+import 'package:go_router/go_router.dart';
+
+class LogoutButton extends StatelessWidget {
+  const LogoutButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.logout),
+      onPressed: () async {
+        // Create an instance of AuthService to call the signOut function
+        AuthService authService = AuthService();
+        await authService.signOut();
+        // Redirect to login page after signing out
+        GoRouter.of(context).go('/login');
+      },
+    );
+  }
+}
